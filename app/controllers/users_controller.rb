ff0163836246e5fd61f0f76a5932df5103f5e3f3
grangerl330 <grangerl330@gmail.com>
2019-03-11 17:01:32 -0400

    updated logout redirect to go to homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
   get '/logout' do
     if logged_in?
       session.clear
-      redirect '/login'
+      redirect '/'
     else
       redirect '/'
     end
