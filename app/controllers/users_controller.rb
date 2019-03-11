efd0722f28a623cc63d5de0dd8d105cfb161d5bb
grangerl330 <grangerl330@gmail.com>
2019-03-11 15:53:21 -0400

    added post /login route to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,17 @@ class UsersController < ApplicationController
       redirect '/cabinets/index'
     else
       erb :'/users/login'
-    end 
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/cabinets/index'
+    else
+      redirect '/login'
+    end
   end
 
 end
