5ed15587ff28d67839d17e6feb6c30b8f97270d6
grangerl330 <grangerl330@gmail.com>
2019-03-11 16:29:06 -0400

    added logout route to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,11 +29,21 @@ class UsersController < ApplicationController
 
   post '/login' do
     @user = User.find_by(:username => params[:username])
+    binding.pry
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect '/cabinets'
     else
       redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect '/login'
+    else
+      redirect '/'
     end
   end
 
