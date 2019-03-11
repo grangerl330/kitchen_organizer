038ce611cb2b7e754c89e3c5ac8c54f06aff00b1
grangerl330 <grangerl330@gmail.com>
2019-03-11 16:59:45 -0400

    updated index.erb in cabinets views to show only current user's cabinets

--- app/controllers/cabinets_controller.rb
+++ app/controllers/cabinets_controller.rb
@@ -2,7 +2,7 @@ class CabinetsController < ApplicationController
 
   get '/cabinets' do
     redirect_if_not_logged_in
-    @cabinets = Cabinet.all
+    @user = User.find_by_id(session[:user_id])
     erb :'/cabinets/index'
   end
 
