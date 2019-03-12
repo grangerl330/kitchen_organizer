b084837fa163d1ce297d284f739c7629cc6acb39
grangerl330 <grangerl330@gmail.com>
2019-03-11 20:56:20 -0400

    added post '/items/ route and updated index.erb and new.erb files in items view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,4 +12,23 @@ class ItemsController < ApplicationController
     erb :'items/new'
   end
 
+  post '/items' do
+    @user = User.find_by_id(session[:user_id])
+    @cabinet = Cabinet.find_by_name(params["cabinet name"])
+
+    if params.value?("")
+      redirect '/items/new'
+    else
+      @item = Item.create(name: params[:name], category: params[:category])
+      @user.items << @item
+      @user.save
+    end
+
+    if @cabinet
+      @cabinet.items << @item
+      @cabinet.save
+    end
+    redirect '/items'
+  end
+
 end
