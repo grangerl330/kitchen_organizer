ac49106b4f558338fe9a66b22ea6b5c170f21487
grangerl330 <grangerl330@gmail.com>
2019-03-13 11:05:02 -0400

    added ability to leave item category blank when creating a new item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,8 +16,8 @@ class ItemsController < ApplicationController
     @user = User.find_by_id(session[:user_id])
     @cabinet = Cabinet.find_by_name(params["cabinet name"])
 
-    if params.value?("")
-      redirect '/items/new'
+    if params["name"] == ""
+      redirect '/items/new?error=Item must have a name'
     else
       @item = Item.create(name: params[:name], category: params[:category])
       @user.items << @item
