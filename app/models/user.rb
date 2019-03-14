aa772f91b35bb53ce49562c0da769885af6eb425
grangerl330 <grangerl330@gmail.com>
2019-03-14 01:14:02 -0400

    removed unecessary methods from User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,4 @@ class User < ActiveRecord::Base
   has_many :cabinets
   has_many :items
   has_secure_password
-
-  def slug
-    username.downcase.gsub(" ", "-")
-  end
-
-  def self.find_by_slug(slug)
-    User.all.find {|user| slug == user.slug}
-  end
 end
