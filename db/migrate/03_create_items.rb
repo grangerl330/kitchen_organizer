7d072966cd5b2129004d285a524583ebfcbfb81f
grangerl330 <grangerl330@gmail.com>
2019-03-13 23:25:30 -0400

    updated database to remove category attribute from items

--- db/migrate/03_create_items.rb
+++ db/migrate/03_create_items.rb
@@ -3,6 +3,7 @@ class CreateItems < ActiveRecord::Migration
     create_table :items do |t|
       t.string :name
       t.integer :cabinet_id
+      t.integer :user_id
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 5) do
+ActiveRecord::Schema.define(version: 3) do
 
   create_table "cabinets", force: :cascade do |t|
     t.string  "name"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 5) do
 
   create_table "items", force: :cascade do |t|
     t.string  "name"
-    t.string  "category"
     t.integer "cabinet_id"
     t.integer "user_id"
   end
